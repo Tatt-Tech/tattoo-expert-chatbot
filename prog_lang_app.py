dd436b4797877d5ad1d7b5c2839b07e8de2e72cb
Darien-B <93536644+Darien-B@users.noreply.github.com>
2023-04-24 22:17:10 -0600

    updates

--- prog_lang_app.py
+++
@@ -1,9 +0,0 @@
-from tattoo_expert_bot import answer_question
-
-def main():
-    question = "Translate the following English text to French: 'Hello, how are you doing?'"
-    response = answer_question(question)
-    print(response)
-
-if __name__ == "__main__":
-    main()
