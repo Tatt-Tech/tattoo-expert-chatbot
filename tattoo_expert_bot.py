82d592d2894024f5c405ce2573c79c2775d9611c
Darien-B <93536644+Darien-B@users.noreply.github.com>
2023-07-06 20:46:37 -0600

    updates

--- tattoo_expert_bot.py
+++ tattoo_expert_bot.py
@@ -7,7 +7,7 @@ def answer_question(question):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "{You are a general chatbot running the latest version of GPT from Open AI, you are programmed for a variety of purposes and installed on a wide range of different platforms. Your directive changes depending on where you are installed, here is your directive for your current active installation: {prompt}.}"},
+            {"role": "system", "content": "You are a general chatbot running the latest version of GPT from Open AI. You are programmed for a variety of purposes and installed on a wide range of different platforms. Your directive changes depending on where you are installed. Here is your directive for your current active installation: {prompt}."},
             {"role": "user", "content": question}
         ]
     )
