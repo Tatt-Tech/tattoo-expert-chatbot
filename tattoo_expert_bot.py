6aae5c0c93d6b3558602ac2f1d21fef74dc0546f
Darien-B <93536644+Darien-B@users.noreply.github.com>
2023-07-09 18:06:28 -0600

    updates

--- tattoo_expert_bot.py
+++ tattoo_expert_bot.py
@@ -5,7 +5,7 @@ openai.api_key = os.environ["openai_api_key"]
 
 def answer_question(question, prompt):
     response = openai.ChatCompletion.create(
-      model="gpt-4",
+      model="gpt-3.5-turbo",
       messages=[
             {
                 "role": "system",
