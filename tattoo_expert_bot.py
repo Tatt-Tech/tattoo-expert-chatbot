461c679c5c3acf72de8bcb8250175c6c125d86e3
Darien-B <93536644+Darien-B@users.noreply.github.com>
2023-07-10 20:35:46 -0600

    updates

--- tattoo_expert_bot.py
+++ tattoo_expert_bot.py
@@ -3,18 +3,18 @@ import os
 
 openai.api_key = os.environ["openai_api_key"]
 
-def answer_question(question, prompt):
+def answer_question(history, prompt):
+    messages = []
+    for message in history:
+        messages.append(openai.ChatCompletion.Message(
+            role=message["role"],
+            content=str(message["content"])  # Ensure the content is a string
+        ))
+
     response = openai.ChatCompletion.create(
-      model="gpt-3.5-turbo",
-      messages=[
-            {
-                "role": "system",
-                "content": prompt  
-            },
-            {
-                "role": "user",
-                "content": question
-            }
-        ]
+        model="gpt-3.5-turbo",
+        messages=messages,
+        max_tokens=150
     )
-    return response.choices[0].message['content']
+
+    return response['choices'][0]['message']['content']
