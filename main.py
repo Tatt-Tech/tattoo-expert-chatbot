80bd4a6c11ecec5cccb42f2f3a8745d9817992c3
Darien-B <93536644+Darien-B@users.noreply.github.com>
2023-07-10 21:30:45 -0600

    updates

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, session, jsonify
+from flask_cors import CORS
 from tattoo_expert_bot import answer_question
 
 app = Flask(__name__)
+CORS(app)
 app.secret_key = 'openai_api_key'
 
 @app.route('/api/chat', methods=['POST'])
