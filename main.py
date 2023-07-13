38cb5fb52563ceabae1069c31edf79b6e1c4e6eb
Darien-B <93536644+Darien-B@users.noreply.github.com>
2023-07-12 22:05:39 -0600

    updates

--- main.py
+++ main.py
@@ -8,28 +8,20 @@ app.secret_key = 'openai_api_key'
 
 @app.route('/api/chat', methods=['POST'])
 def chat():
-    # Load the question and prompt from the POST request
+    # Load the question, prompt, and history from the POST request
     data = request.get_json()
     question = data['message']
     prompt = data.get('prompt', '')  # The prompt is optional
+    history = data.get('history', [])  # The history is optional
 
-    # If 'history' is not in the session, initialize it with the prompt
-    # This happens the first time the function is called
-    if 'history' not in session:
-        session['history'] = [{'role': 'system', 'content': prompt}] if prompt else []
-        print('Initializing session history')  # Debug line
-
-    print('Current session history:', session['history'])  # Debug line
+    print('Current session history:', history)  # Debug line
 
     # Call the function to generate the answer
-    answer = answer_question(session['history'], question)
+    answer = answer_question(history, question)
 
     # Add the question and answer to the history
-    session['history'].append({'role': 'user', 'content': question})
-    session['history'].append({'role': 'assistant', 'content': answer})
-
-    # Save the session
-    session.modified = True
+    history.append({'role': 'user', 'content': question})
+    history.append({'role': 'assistant', 'content': answer})
 
     # Return the answer
     return jsonify({'message': answer})
