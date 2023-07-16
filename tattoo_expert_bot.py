6e25c847812026c835ca4105f305b7da80d0ae9e
Darien-B <93536644+Darien-B@users.noreply.github.com>
2023-07-16 13:55:52 -0600

    updates

--- tattoo_expert_bot.py
+++ tattoo_expert_bot.py
@@ -26,14 +26,8 @@ def answer_question(history, question, prompt):
     phone_pattern = re.compile(r'\b(\+\d{1,2}\s?)?1?\-?\.?\s?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\b')
 
     # Replace URLs, emails, and phone numbers with clickable links
-    answer = url_pattern.sub(r'<a href="\g<0>">\g<0></a>', answer)
-    answer = email_pattern.sub(r'<a href="mailto:\g<0>">\g<0></a>', answer)
-    answer = phone_pattern.sub(r'<a href="tel:\g<0>">\g<0></a>', answer)
-
-    # General regex pattern to find incorrectly formatted HTML anchor tags
-    incorrect_anchor_pattern = re.compile(r'"(http[s]?://[^"]+)" target="_blank">([^<]+)')
-
-    # Replace any incorrectly formatted anchor tags with correct HTML
-    answer = incorrect_anchor_pattern.sub(r'<a href="\1" target="_blank">\2</a>', answer)
+    answer = url_pattern.sub(r'<a href="\g<0>" target="_blank">\g<0></a>', answer)
+    answer = email_pattern.sub(r'<a href="mailto:\g<0>" target="_blank">\g<0></a>', answer)
+    answer = phone_pattern.sub(r'<a href="tel:\g<0>" target="_blank">\g<0></a>', answer)
 
     return answer
