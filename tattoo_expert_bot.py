0f715e87966087e76be232af1b3c0514de596c51
Darien-B <93536644+Darien-B@users.noreply.github.com>
2023-07-16 13:21:32 -0600

    updates

--- tattoo_expert_bot.py
+++ tattoo_expert_bot.py
@@ -1,5 +1,6 @@
 import openai
 import os
+import re
 
 openai.api_key = os.environ["openai_api_key"]
 
@@ -16,4 +17,17 @@ def answer_question(history, question, prompt):
       messages=messages
     )
 
-    return response['choices'][0]['message']['content']
+    # Get the response message
+    answer = response['choices'][0]['message']['content']
+
+    # Define regex patterns
+    url_pattern = re.compile(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+    email_pattern = re.compile(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b')
+    phone_pattern = re.compile(r'\b(\+\d{1,2}\s?)?1?\-?\.?\s?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\b')
+
+    # Replace URLs, emails, and phone numbers with clickable links
+    answer = url_pattern.sub(r'<a href="\g<0>">\g<0></a>', answer)
+    answer = email_pattern.sub(r'<a href="mailto:\g<0>">\g<0></a>', answer)
+    answer = phone_pattern.sub(r'<a href="tel:\g<0>">\g<0></a>', answer)
+
+    return answer
